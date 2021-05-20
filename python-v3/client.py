6de2d6d32986683414b6c31fe0e85922957ffcb3
saddathasan <saddathasan94@gmail.com>
2021-05-21 01:27:39 +0600

    added the python v3 files

---
+++ python-v3/client.py
@@ -0,0 +1,7 @@
+import socket   # for using the socket module for network and TCP protocol
+import tqdm     # for loading bar and stuffs in the terminal 
+import os       # for generic OS module operations and stuffs 
+
+SEPARATOR = "<SEPARATOR>"
+
+BUTTER_SIZE = 4096  # this is to define that every time 4096 byte will be sent
